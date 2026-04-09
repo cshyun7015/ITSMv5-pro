20fc1344c90c5d5dfb433e545d9735992d0b44ed
cshyun7015 <cshyun7015@gmail.com>
2026-04-09 23:37:49 +0900

    시스템 백앤드 래팩토링

--- backend-system/src/test/java/com/itsm/system/controller/operator/mapping/TeamCustomerMapControllerTest.java
+++ backend-system/src/test/java/com/itsm/system/controller/operator/mapping/TeamCustomerMapControllerTest.java
@@ -50,7 +50,7 @@ class TeamCustomerMapControllerTest {
         given(mappingService.getMappingsByTeam(1L)).willReturn(List.of(dto));
 
         // when & then
-        mockMvc.perform(get("/api/v1/organization/mappings/team/1"))
+        mockMvc.perform(get("/v1/mapping/team/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].operatorTeamId").value(1))
                 .andExpect(jsonPath("$[0].customerCompanyName").value("고객사A"));
@@ -69,7 +69,7 @@ class TeamCustomerMapControllerTest {
         given(mappingService.getMappingsByCustomer(10L)).willReturn(List.of(dto));
 
         // when & then
-        mockMvc.perform(get("/api/v1/organization/mappings/customer/10"))
+        mockMvc.perform(get("/v1/mapping/customer/10"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].customerCompanyId").value(10))
                 .andExpect(jsonPath("$[0].operatorTeamName").value("운영1팀"));
@@ -86,7 +86,7 @@ class TeamCustomerMapControllerTest {
         given(mappingService.assignTeamToCustomer(1L, 10L)).willReturn(dto);
 
         // when & then
-        mockMvc.perform(post("/api/v1/organization/mappings/1/10"))
+        mockMvc.perform(post("/v1/mapping/1/10"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.operatorTeamId").value(1))
                 .andExpect(jsonPath("$.customerCompanyId").value(10));
@@ -99,7 +99,7 @@ class TeamCustomerMapControllerTest {
         doNothing().when(mappingService).unassignTeamFromCustomer(1L, 10L);
 
         // when & then
-        mockMvc.perform(delete("/api/v1/organization/mappings/1/10"))
+        mockMvc.perform(delete("/v1/mapping/1/10"))
                 .andExpect(status().isNoContent());
     }
 }
